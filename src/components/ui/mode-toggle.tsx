3ac047681bbcf4258d32df02df51cbda50b01175
Vitali Haradkou <8816260+vitalics@users.noreply.github.com>
2026-01-19 14:30:48 +0300

    fix: update hero image for playwright labs

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,41 +1,43 @@
-import { Button } from '@/components/ui/button'
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Laptop, Moon, Sun } from 'lucide-react'
-import * as React from 'react'
+} from "@/components/ui/dropdown-menu";
+import { Laptop, Moon, Sun } from "lucide-react";
+import * as React from "react";
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
-    'theme-light' | 'dark' | 'system'
-  >('theme-light')
+    "theme-light" | "dark" | "system"
+  >("theme-light");
 
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains('dark')
-    setThemeState(isDarkMode ? 'dark' : 'theme-light')
-  }, [])
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setThemeState(isDarkMode ? "dark" : "theme-light");
+  }, []);
 
   React.useEffect(() => {
     const isDark =
-      theme === 'dark' ||
-      (theme === 'system' &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
+      theme === "dark" ||
+      (theme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
 
-    document.documentElement.classList.add('disable-transitions')
+    document.documentElement.classList.add("disable-transitions");
 
-    document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
 
     window
       .getComputedStyle(document.documentElement)
-      .getPropertyValue('opacity')
+      .getPropertyValue("opacity");
 
     requestAnimationFrame(() => {
-      document.documentElement.classList.remove('disable-transitions')
-    })
-  }, [theme])
+      document.documentElement.classList.remove("disable-transitions");
+    });
+  }, [theme]);
 
   return (
     <DropdownMenu modal={false}>
@@ -52,19 +54,19 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="bg-background">
-        <DropdownMenuItem onClick={() => setThemeState('theme-light')}>
+        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
           <Sun className="mr-2 size-4" />
           <span>Light</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState('dark')}>
+        <DropdownMenuItem onClick={() => setThemeState("dark")}>
           <Moon className="mr-2 size-4" />
           <span>Dark</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState('system')}>
+        <DropdownMenuItem onClick={() => setThemeState("system")}>
           <Laptop className="mr-2 size-4" />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
